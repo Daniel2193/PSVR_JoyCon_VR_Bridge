b49340f0631aa22c83bbb371ec58e9aa109efb47
Daniel2193 <daniel2193mc@gmail.com>
2020-03-31 14:39:47 +0200

    Added FreePieIO Module Project

---
+++ FreePieIO_Module/Program.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FreePieIO_Module
+{
+    class Program
+    {
+        /* Todo list
+        Move JoyCon connection/communication to this project
+        Find a good position for the Raspberry Pi to improve position tracking accuracy
+        Add/implement a second Raspberry Pi
+        */
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Data
+        {
+            public float Yaw;
+            public float Pitch;
+            public float Roll;
+
+            public float X;
+            public float Y;
+            public float Z;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetDllDirectory(string lpPathName);
+
+        [DllImport("freepie_io.dll")]
+        private static extern int freepie_io_6dof_slots();
+
+        [DllImport("freepie_io.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int freepie_io_6dof_read(int index, int length, out Data data);
+
+        [DllImport("freepie_io.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int freepie_io_6dof_write(int index, int length, Data[] data);
+
+        //0 -> Controller buttons   1 -> Headset   2 -> Left Controller   3 -> Right Controller
+        private const int WriteToIndex = 1;
+
+        //Multiplier
+        static double x1m = 0.02;
+        static double x2m = 1;
+        static double x3m = 1;
+        static double y1m = -0.02;
+        static double y2m = 1;
+        static double y3m = 1;
+        static double z1m = 0.02;
+        static double z2m = 1;
+        static double z3m = 1;
+        static double roll2m = 1;
+        static double roll3m = 1;
+        static double pitch2m = 1;
+        static double pitch3m = 1;
+        static double yaw2m = 1;
+        static double yaw3m = 1;
+
+
+        //Values
+        static double X1 = 0;
+        static double X2 = 0;
+        static double X3 = 0;
+        static double Y1 = 0;
+        static double Y2 = 0;
+        static double Y3 = 0;
+        static double Z1 = 0;
+        static double Z2 = 0;
+        static double Z3 = 0;
+        static double ROLL2 = 0;
+        static double ROLL3 = 0;
+        static double PITCH2 = 0;
+        static double PITCH3 = 0;
+        static double yAW2 = 0;
+        static double YAW3 = 0;
+
+        static void Main(string[] args)
+        {
+            SetFreePIEDllPath();
+
+            
+            Task.Run(async () =>
+            {
+                using (var udp = new UdpClient(2193))
+                {
+                    string[] data;
+                    while (true)
+                    {
+                        //This UdpClient rreceives Data from a Raspberry Pi
+                        var rec = await udp.ReceiveAsync();
+                        string sup = Encoding.ASCII.GetString(rec.Buffer);
+                        data = Regex.Split(sup, "lol");
+
+                        //At the moment im only transfering 3 numbers (double)
+                        X1 = Convert.ToDouble(data[0]) * x1m;
+                        Y1 = Convert.ToDouble(data[1]) * y1m;
+                        Z1 = (Convert.ToDouble(data[2]) * z1m) - 2;
+                    }
+                }
+            });
+
+
+            var yaw = 3f;
+            var arr = new Data[1];
+
+            while (true)
+            {
+                arr[0] = new Data
+                {
+                    Yaw = yaw,
+                    X = (float) X1,
+                    Y = (float) Y1,
+                    Z = (float) Z1
+                };
+                yaw = yaw + 0.00001f;
+                if (yaw > 180)
+                    yaw = -180;
+
+                var result = freepie_io_6dof_write(WriteToIndex, 1, arr);
+                if (result != 0)
+                    throw new Exception(string.Format("Could not write to slot {0}", WriteToIndex));
+
+            }
+        }
+
+        private static void SetFreePIEDllPath()
+        {
+            var path = Registry.GetValue(string.Format("{0}\\Software\\{1}", Registry.CurrentUser, "FreePIE"), "path", null) as string;
+            SetDllDirectory(path);
+            Console.WriteLine("Init done");
+        }
+    }
+}
---
+++ FreePieIO_Module/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("FreePieIO_Module")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FreePieIO_Module")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("a24158bc-6d48-4154-ace8-1d035a17c968")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
