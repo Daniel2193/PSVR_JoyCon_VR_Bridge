f4cfd1baeb289c3ffa0690c508b537718a76bafd
Daniel2193 <daniel2193mc@gmail.com>
2020-05-19 01:53:25 +0200

    Added Relative Positioning

--- FreePieIO_Module/Program.cs
+++ FreePieIO_Module/Program.cs
@@ -29,6 +29,12 @@ namespace FreePieIO_Module
             public float Z;
         }
 
+        private enum AngleType
+        {
+            Degrees = 0,
+            Radiant = 1
+        }
+
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool SetDllDirectory(string lpPathName);
@@ -82,8 +88,8 @@ namespace FreePieIO_Module
 
         //Joycons
         static JoyconManager joyconManager = new JoyconManager();
-        static int DriftSamples = 20;
-        static int DriftSampleDelay = 500;
+        static int L = -1;
+        static int R = -1;
         static bool[] calibFinished;
         static double DriftYawL = 0;
         static double DriftPitchL = 0;
@@ -92,11 +98,26 @@ namespace FreePieIO_Module
         static double DriftPitchR = 0;
         static double DriftRollR = 0;
         static double[] DriftYawOffset;
-
+        static int DriftSamples = 200;
+        static int DriftSampleDelay = 50;
         static bool DriftFilterOnAllAxis = false;
 
-        //Packet
+        static bool skipCalib = false;
+
+        static double distance = 0.3;
+        static double yawOffset = 180;
+        static double yawMultiplier = -1;
+        static double pitchOffset = 90;
+        static double pitchMultiplier = 1;
+        static double rollOffset = 0;
+        static double rollMultiplier = 1;
+        static Vector3[] JoyconPosition;
+        static Vector3[] HomePosition;
+
+
+        //Packet array
         static Data[] packets;
+        static Data headset = new Data();
         /*
          * Controller Button Packet / Slot 0:
          * 
@@ -130,6 +151,12 @@ namespace FreePieIO_Module
 
             packets = new Data[4];
             DriftYawOffset = new double[2];
+            JoyconPosition = new Vector3[2];
+            JoyconPosition[0] = new Vector3(0, 0, 0);
+            JoyconPosition[1] = new Vector3(0, 0, 0);
+            HomePosition = new Vector3[2];
+            HomePosition[0] = new Vector3(0, 0, 0);
+            HomePosition[1] = new Vector3(0, 0, 0);
 
             joyconManager.Scan();
 
@@ -147,17 +174,38 @@ namespace FreePieIO_Module
                 Console.WriteLine("Calibrating Sensors -> leave Joycons on a flat surface until calibration is finished");
                 Console.WriteLine("To Start the calibration press ENTER/RETURN");
                 Console.WriteLine("Awaiting calibration...");
-                calibFinished = new bool[2];
-                calibFinished[0] = false;
-                calibFinished[1] = false;
-                Console.ReadLine();
-                Console.WriteLine("Beginning calibration. This will take " + (DriftSamples * DriftSampleDelay / 1000) + " seconds");
-                calibration(0);
-                calibration(1);
-                Thread.Sleep(DriftSamples*DriftSampleDelay+500);
-                resetOrientation(0);
-                resetOrientation(1);
-                Console.WriteLine("Calibration complete");
+                if (joyconManager.j[0].isLeft)
+                {
+                    L = 0;
+                    R = 1;
+                }
+                else
+                {
+                    L = 1;
+                    R = 0;
+                }
+                if (!skipCalib)
+                {
+                    calibFinished = new bool[2];
+                    calibFinished[0] = false;
+                    calibFinished[1] = false;
+                    Console.ReadLine();
+                    Console.WriteLine("Beginning calibration. This will take " + (DriftSamples * DriftSampleDelay / 1000) + " seconds");
+                    calibration(L);
+                    calibration(R);
+                    Thread.Sleep(DriftSamples * DriftSampleDelay + 500);
+                    resetOrientation(L);
+                    resetOrientation(R);
+                    Console.WriteLine("Calibration complete");
+                }
+                else
+                {
+                    Console.WriteLine("!!!!! CALIBRATION SKIPPED !!!!!");
+                    calibFinished = new bool[2];
+                    calibFinished[0] = true;
+                    calibFinished[1] = true;
+                }
+                
 
 
             }
@@ -187,16 +235,16 @@ namespace FreePieIO_Module
             DateTime tick = DateTime.Now;
             while (true)
             {
-                if (joyconManager.j[0].GetButtonDown(Joycon.Button.CAPTURE))
-                    resetOrientation(0);
-                if (joyconManager.j[1].GetButtonDown(Joycon.Button.HOME))
-                    resetOrientation(1);
+                if (joyconManager.j[L].GetButtonDown(Joycon.Button.CAPTURE))
+                    resetOrientation(L);
+                if (joyconManager.j[R].GetButtonDown(Joycon.Button.HOME))
+                    resetOrientation(R);
                 packets[0] = new Data
                 {
-                    Yaw = ((joyconManager.j[1].GetButton(Joycon.Button.PLUS) ? 1:0) << 24) + ((joyconManager.j[0].GetButton(Joycon.Button.MINUS) ? 1 : 0) << 16) + ((joyconManager.j[1].GetButton(Joycon.Button.SHOULDER_2) ? 255 : 0) << 8) + (joyconManager.j[0].GetButton(Joycon.Button.SHOULDER_2) ? 255 : 0),
-                    Pitch = ((joyconManager.j[1].GetButton(Joycon.Button.SL) ? 1 : 0) << 24) + ((joyconManager.j[0].GetButton(Joycon.Button.SR) ? 1 : 0) << 16) + ((joyconManager.j[1].GetButton(Joycon.Button.DPAD_LEFT) ? 1 : 0) << 8) + (joyconManager.j[0].GetButton(Joycon.Button.DPAD_LEFT) ? 1 : 0),
-                    Roll = ((joyconManager.j[1].GetButton(Joycon.Button.STICK) ? 1 : 0) << 8) + (joyconManager.j[0].GetButton(Joycon.Button.STICK) ? 1 : 0),
-                    X = ((int)((joyconManager.j[1].GetStick()[1] + 0.03f)* 0.5f * 255 + 0.5f * 255) << 24) + ((int)(joyconManager.j[0].GetStick()[1] * 0.5f * 255 + 0.5f * 255) << 16) + ((int)((joyconManager.j[0].GetStick()[1] + 0.03f) * 0.5f * 255 + 0.5f * 255) << 8) + (int)(joyconManager.j[0].GetStick()[0] * 0.5f * 255 + 0.5f * 255)
+                    Yaw = ((joyconManager.j[R].GetButton(Joycon.Button.PLUS) ? 1:0) << 24) + ((joyconManager.j[L].GetButton(Joycon.Button.MINUS) ? 1 : 0) << 16) + ((joyconManager.j[R].GetButton(Joycon.Button.SHOULDER_2) ? 255 : 0) << 8) + (joyconManager.j[L].GetButton(Joycon.Button.SHOULDER_2) ? 255 : 0),
+                    Pitch = ((joyconManager.j[R].GetButton(Joycon.Button.SL) ? 1 : 0) << 24) + ((joyconManager.j[L].GetButton(Joycon.Button.SR) ? 1 : 0) << 16) + ((joyconManager.j[R].GetButton(Joycon.Button.DPAD_LEFT) ? 1 : 0) << 8) + (joyconManager.j[L].GetButton(Joycon.Button.DPAD_LEFT) ? 1 : 0),
+                    Roll = ((joyconManager.j[R].GetButton(Joycon.Button.STICK) ? 1 : 0) << 8) + (joyconManager.j[L].GetButton(Joycon.Button.STICK) ? 1 : 0),
+                    X = ((int)((joyconManager.j[R].GetStick()[1] + 0.03f)* 0.5f * 255 + 0.5f * 255) << 24) + ((int)(joyconManager.j[L].GetStick()[1] * 0.5f * 255 + 0.5f * 255) << 16) + ((int)((joyconManager.j[R].GetStick()[1] + 0.03f) * 0.5f * 255 + 0.5f * 255) << 8) + (int)(joyconManager.j[L].GetStick()[0] * 0.5f * 255 + 0.5f * 255)
                 };
                 packets[1] = new Data
                 {
@@ -204,9 +252,13 @@ namespace FreePieIO_Module
                     Y = (float) Y1,
                     Z = (float) Z1
                 };
-                YAW2 = (getYaw(0) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftYawL) - DriftYawOffset[0]);
-                PITCH2 = (getPitch(0) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftPitchL));
-                ROLL2 = (getRoll(0) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftRollL));
+                YAW2 = (getYaw(L) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftYawL) - DriftYawOffset[L]);
+                PITCH2 = (getPitch(L) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftPitchL));
+                ROLL2 = (getRoll(L) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftRollL));
+                calculatePosition(L);
+                X2 = JoyconPosition[L].X;
+                Y2 = JoyconPosition[L].Y;
+                Z2 = JoyconPosition[L].Z;
                 packets[2] = new Data
                 {
                     X = (float)X2,
@@ -216,9 +268,13 @@ namespace FreePieIO_Module
                     Pitch = (float) PITCH2,
                     Roll = (float) ROLL2
                 };
-                YAW3 = (getYaw(1) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftYawR) - DriftYawOffset[1]);
-                PITCH3 = (getPitch(1) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftPitchR));
-                ROLL3 = (getRoll(1) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftRollR));
+                YAW3 = (getYaw(R) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftYawR) - DriftYawOffset[R]);
+                PITCH3 = (getPitch(R) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftPitchR));
+                ROLL3 = (getRoll(R) - (DateTime.Now.Subtract(tick).TotalSeconds * DriftRollR));
+                calculatePosition(R);
+                X3 = JoyconPosition[R].X;
+                Y3 = JoyconPosition[R].Y;
+                Z3 = JoyconPosition[R].Z;
                 packets[3] = new Data
                 {
                     X = (float) X3,
@@ -231,13 +287,26 @@ namespace FreePieIO_Module
 
                 var result = freepie_io_6dof_write(0, 4, packets);
                 if (result != 0)
-                    throw new Exception("Could not write to IO slots");
+                    throw new Exception("Could not write to IO slots. Try reinstalling FreePie and reboot your PC");
+                var res = freepie_io_6dof_read(1, 1, out headset);
             }
+        }
+
+        public static void calculatePosition(int index){
+            Vector3 tmp = Vector3.Zero;
+            
+            double b = ((getYaw(index) - yawOffset) * -1) / 360 * 2 * Math.PI;
+            double a = (getPitch(index))/ 360 * 2 * Math.PI;
+            tmp.X = HomePosition[index].X + distance * Math.Cos(a) * Math.Sin(b);
+            tmp.Y = HomePosition[index].Y + distance * Math.Sin(a);
+            tmp.Z = HomePosition[index].Z + distance * Math.Cos(a) * Math.Cos(b);
+            
+            JoyconPosition[index] = tmp;
         }
 
         private static void resetOrientation(int index)
         {
-            DriftYawOffset[index] = getYaw(index);
+            DriftYawOffset[index] = getRawYaw(index);
         }
 
         private static void calibration(int index)
@@ -248,15 +317,15 @@ namespace FreePieIO_Module
                 double pitch = 0;
                 double roll = 0;
 
-                double lastYaw = getYaw(index);
+                double lastYaw = getRawYaw(index);
                 double lastPitch = getPitch(index);
                 double lastRoll = getRoll(index);
 
                 for (int i = 0; i < DriftSamples; i++)
                 {
                     await Task.Delay(DriftSampleDelay);
-                    yaw += (getYaw(index) - lastYaw);
-                    lastYaw = getYaw(index);
+                    yaw += (getRawYaw(index) - lastYaw);
+                    lastYaw = getRawYaw(index);
                     pitch += (getPitch(index) - lastPitch);
                     lastPitch = getPitch(index);
                     roll += (getRoll(index) - lastRoll);
@@ -265,9 +334,9 @@ namespace FreePieIO_Module
                 double avgYaw = (yaw / DriftSamples) * (1000 / DriftSampleDelay);
                 double avgPitch = (pitch / DriftSamples) * (1000 / DriftSampleDelay);
                 double avgRoll = (roll / DriftSamples) * (1000 / DriftSampleDelay);
-                Console.WriteLine(Environment.NewLine + "AVG Drift on " + (index == 0 ? "left" : "right") + " Yaw: " + avgYaw + "°/sec");
-                Console.WriteLine(Environment.NewLine + "AVG Drift on " + (index == 0 ? "left" : "right") + " Pitch: " + avgPitch + "°/sec");
-                Console.WriteLine(Environment.NewLine + "AVG Drift on " + (index == 0 ? "left" : "right") + " Roll: " + avgRoll + "°/sec");
+                Console.WriteLine(Environment.NewLine + "AVG Drift on " + (index == L ? "left" : "right") + " Yaw: " + avgYaw + "°/sec");
+                Console.WriteLine(Environment.NewLine + "AVG Drift on " + (index == L ? "left" : "right") + " Pitch: " + avgPitch + "°/sec");
+                Console.WriteLine(Environment.NewLine + "AVG Drift on " + (index == L ? "left" : "right") + " Roll: " + avgRoll + "°/sec");
                 if(index == 0)
                 {
                     DriftYawL = avgYaw;
@@ -300,17 +369,34 @@ namespace FreePieIO_Module
             SetDllDirectory(path);
         }
 
+        private static double getRawYaw(int index)
+        {
+            return (joyconManager.j[index].GetVector().eulerAngles.Z * 180 / Math.PI * yawMultiplier);
+        }
         private static double getRoll(int index)
         {
             return (joyconManager.j[index].GetVector().eulerAngles.X * 180 / Math.PI);
         }
         private static double getPitch(int index)
         {
-            return (joyconManager.j[index].GetVector().eulerAngles.Y * 180 / Math.PI - 180);
+            return (joyconManager.j[index].GetVector().eulerAngles.Y * 180 / Math.PI + pitchOffset);
         }
         private static double getYaw(int index)
         {
-            return (joyconManager.j[index].GetVector().eulerAngles.Z * 180 / Math.PI * -1);
+            return (joyconManager.j[index].GetVector().eulerAngles.Z * 180 / Math.PI * yawMultiplier + yawOffset);
+        }
+
+        private static double fixAngle(double input, AngleType type)
+        {
+            if(type == AngleType.Degrees)
+            {
+                return input % 360;
+            }
+            else if(type == AngleType.Radiant)
+            {
+                return input % (2 * Math.PI);
+            }
+            return input;
         }
     }
 }
